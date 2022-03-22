ed7f960d9b58f785b7e321b07c725275caf3cd29
Mohit kumar <50844220+itsmohitkr@users.noreply.github.com>
2022-03-22 08:13:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("1. printing process");
+
+console.log(process);
+
+console.log("2. printing process.env");
+console.log(process.env);
+
+
+console.log("3. printing process.argv");
+console.log(process.argv);
+
+
